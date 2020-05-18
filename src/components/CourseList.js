ff36d8d3c0a321a0b1d63000fcd0a5fd71714432
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-05-18 20:25:44 +0200

    adding the react proyect to git hub

---
+++ src/components/CourseList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+const CourseList = ({ courses }) => (
+  <table className="table">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {courses.map((course) => {
+        return (
+          <tr key={course.id}>
+            <td>{course.title}</td>
+            <td>{course.authorId}</td>
+            <td>{course.category}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
+);
+
+CourseList.propTypes = {
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default CourseList;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+  <div className="jumbotron">
+    <h1>Pluralsight Administration</h1>
+    <p>React, Flux, and React Router for ultra-responsive web app.</p>
+    <Link to="About" className="btn btn-primary">
+      About
+    </Link>
+  </div>
+);
+export default HomePage;
