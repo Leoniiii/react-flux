314f87ece6661525c4f1ce81394918530008e294
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-05-19 12:07:45 +0200

    Adding a form in the ManageCourseComponent

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Prompt } from "react-router-dom";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = (props) => (
   <>
     <Prompt when={true} message="Are you sure" />
     <h2>Manage Course</h2>
-
-    {props.match.params.test}
+    <CourseForm />
   </>
 );
 export default ManageCoursePage;
